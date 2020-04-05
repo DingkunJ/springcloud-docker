3aff7e6f4b6dbaacf054901e660e3051d26eb6df
dingkunj <dingkunj@outlook.com>
2020-04-05 21:14:26 +0800

    1.0

--- package.sh
+++ package.sh
@@ -1,31 +1,22 @@
 read -r -p "是否自动停止容器并删除springcloud-eureka相关镜像? [y/n] " input
 
-if [[ $input == "Y" || $input == "y" ]] 
-then 
-    # 停止已有容器
-    for m in $(docker ps -a | awk '{print $2}' | sed -n '2,$p')
-    do
-        result=$(echo $m | grep "springcloud-eureka")   # 这里的等号不能有空格
-        if [[ $result != "" ]]                          # 这里的等号要有空格
-        then
-            mid=$(docker ps -a | grep $m | awk '{print $1}')
-            echo $mid
-            docker rm -f $m
-        fi
-    done
-
+if [[ $input == "Y" || $input == "y" ]]
+then
     # 移除原有docker镜像
     for im in $(docker images | awk '{print $1}' | sed -n '2,$p')
     do
-        result=$(echo $im | grep "springcloud-eureka") 
-        if [[ $result != "" ]]              
+        result=$(echo $im | grep "springcloud-eureka")
+        if [[ $result != "" ]]
         then
             imid=$(docker images | grep $im | awk '{print $3}')
+            cmid=$(docker ps -a -q | grep $imid)
+            docker rm -f $cmid
             docker rmi -f $imid
             echo $im" has been removed"
         fi
     done
 fi
+
 
 read -r -p "请输入项目目录" dirpath
 
